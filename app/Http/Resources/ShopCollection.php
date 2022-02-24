25fa9820de8cd2c0e762287d59a7acb039cdfd25
sujan-sht <sujanstha016@gmail.com>
2022-02-24 13:55:21 +0545

    product details api

--- app/Http/Resources/ShopCollection.php
+++ app/Http/Resources/ShopCollection.php
@@ -8,16 +8,20 @@ class ShopCollection extends ResourceCollection
 {
     public function toArray($request)
     {
+
         return [
             'data' => $this->collection->map(function($data) {
+                echo '<pre>';
+                print_r($data->user);
+                echo '</pre>';
                 return [
                     'name' => $data->name,
-                    'user' => [
-                        'name' => $data->user->name,
-                        'email' => $data->user->email,
-                        'avatar' => $data->user->avatar,
-                        'avatar_original' => $data->user->avatar_original
-                    ],
+                    // 'user' => [
+                    //     'name' => $data->user->name,
+                    //     'email' => $data->user->email,
+                    //     'avatar' => $data->user->avatar,
+                    //     'avatar_original' => $data->user->avatar_original
+                    // ],
                     'logo' => $data->logo,
                     'sliders' => json_decode($data->sliders),
                     'address' => $data->address,
