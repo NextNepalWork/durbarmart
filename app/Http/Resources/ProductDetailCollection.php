5e7b6aa882e105132521d2c0560397791dd50d20
sujan-sht <sujanstha016@gmail.com>
2022-03-02 16:21:02 +0545

    base url removed

--- app/Http/Resources/ProductDetailCollection.php
+++ app/Http/Resources/ProductDetailCollection.php
@@ -14,7 +14,7 @@ class ProductDetailCollection extends ResourceCollection
         return [
             'data' => $this->collection->map(function($data) {
                 $photo=[];
-                $placeholder_img=asset('frontend/images/placeholder.jpg');
+                $placeholder_img='uploads/frontend/images/placeholder.jpg';
                 if(isset($data->photos)){
                     array_push($photo,$placeholder_img);
                 }else{
@@ -63,9 +63,9 @@ class ProductDetailCollection extends ResourceCollection
                         ]
                     ],
                     'photos' => $photo,
-                    'thumbnail_image' => file_exists($data->thumbnail_img) ? $data->thumbnail_img : asset('frontend/images/placeholder.jpg'),
-                    'featured_image' => file_exists($data->featured_img) ? $data->featured_img : asset('frontend/images/placeholder.jpg'),
-                    'flash_deal_image' => file_exists($data->flash_deal_img) ? $data->flash_deal_img : asset('frontend/images/placeholder.jpg'),
+                    'thumbnail_image' => file_exists($data->thumbnail_img) ? $data->thumbnail_img : $placeholder_img,
+                    'featured_image' => file_exists($data->featured_img) ? $data->featured_img : $placeholder_img,
+                    'flash_deal_image' => file_exists($data->flash_deal_img) ? $data->flash_deal_img : $placeholder_img,
                     'tags' => explode(',', $data->tags),
                     'unit_price' => $data->unit_price,
                     'price_lower' => (double) explode('-', homeDiscountedPrice($data->id))[0],
