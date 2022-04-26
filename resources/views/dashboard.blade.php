df6b6349ce97a8d4297527d2663db320a5dc6123
Bipin Joshi <joshibipin2052@gmail.com>
2022-04-26 10:51:19 +0545

    check

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,95 @@
         </div>
     </div>
 @endif
+@if(Auth::user()->user_type == 'admin' || in_array('1', json_decode(Auth::user()->staff->role->permissions)))
+<div class="row">
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-body text-center dash-widget dash-widget-left">
+                <div class="dash-widget-vertical">
+                    <div class="rorate">PRODUCTS</div>
+                </div>
+                <div class="pad-ver mar-top text-main">
+                    <i class="demo-pli-data-settings icon-4x"></i>
+                </div>
+                <br>
+                <p class="text-lg text-main">
+                    Total published products: 
+                    <span class="text-bold">
+                        {{-- {{ \App\Product::where('published', 1)->get()->count() }} --}}
+                        @php
+                            $count = \App\Product::where('published', 1)->get()->count();
+                            echo $count; 
+                        @endphp
+                    </span>
+                </p>
+                @if (\App\BusinessSetting::where('type', 'vendor_system_activation')->first()->value == 1)
+                    <p class="text-lg text-main">
+                        Total sellers products: 
+                        <span class="text-bold">
+                            @php
+                                $count = \App\Product::where('published', 1)->where('added_by', 'seller')->get()->count();
+                                echo $count; 
+                            @endphp
+                            {{-- {{ \App\Product::where('published', 1)->where('added_by', 'seller')->get()->count() }} --}}
+                        </span>
+                    </p>
+                @endif
+                <p class="text-lg text-main">
+                    Total admin products: 
+                    <span class="text-bold">
+                        @php
+                            $count = \App\Product::where('published', 1)->where('added_by', 'admin')->get()->count();
+                            echo $count; 
+                        @endphp
+                        {{-- {{ \App\Product::where('published', 1)->where('added_by', 'admin')->get()->count() }} --}}
+                    </span>
+                </p>
+                <br>
+                <a href="{{ route('products.admin') }}" class="btn btn-primary mar-top">
+                    Manage Products 
+                    <i class="fa fa-long-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="panel">
+                    <div class="pad-top text-center dash-widget">
+                        <p class="text-normal text-main">{{__('Total product category')}}</p>
+                        <p class="text-semibold text-3x text-main">{{ \App\Category::all()->count() }}</p>
+                        <a href="{{ route('categories.create') }}" class="btn btn-primary mar-top btn-block top-border-radius-no">{{__('Create Category')}}</a>
+                    </div>
+                </div>
+                <div class="panel">
+                    <div class="pad-top text-center dash-widget">
+                        <p class="text-normal text-main">{{__('Total product sub sub category')}}</p>
+                        <p class="text-semibold text-3x text-main">{{ \App\SubSubCategory::all()->count() }}</p>
+                        <a href="{{ route('subsubcategories.create') }}" class="btn btn-primary mar-top btn-block top-border-radius-no">{{__('Create Sub Sub Category')}}</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="panel">
+                    <div class="pad-top text-center dash-widget">
+                        <p class="text-normal text-main">{{__('Total product sub category')}}</p>
+                        <p class="text-semibold text-3x text-main">{{ \App\SubCategory::all()->count() }}</p>
+                        <a href="{{ route('subcategories.create') }}" class="btn btn-primary mar-top btn-block top-border-radius-no">{{__('Create Sub Category')}}</a>
+                    </div>
+                </div>
+                <div class="panel">
+                    <div class="pad-top text-center dash-widget">
+                        <p class="text-normal text-main">{{__('Total product brand')}}</p>
+                        <p class="text-semibold text-3x text-main">{{ \App\Brand::all()->count() }}</p>
+                        <a href="{{ route('brands.create') }}" class="btn btn-primary mar-top btn-block top-border-radius-no">{{__('Create Brand')}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 
 @if((Auth::user()->user_type == 'admin' || in_array('5', json_decode(Auth::user()->staff->role->permissions))) && \App\BusinessSetting::where('type', 'vendor_system_activation')->first()->value == 1)
     <div class="row">
